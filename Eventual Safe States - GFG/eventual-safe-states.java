c7a6c513e9d9ce83339cefd30c59d86347507f34
Vernika Agarwal <67332514+vernika302000@users.noreply.github.com>
2023-12-30 13:08:15 +0530

    Added solution - LeetHub

---
+++ Eventual Safe States - GFG/eventual-safe-states.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+
+            List<List<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>());
+            }
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+
+                adj.get(u).add(v);
+            }
+
+            Solution obj = new Solution();
+            List<Integer> safeNodes = obj.eventualSafeNodes(V, adj);
+            for (int i : safeNodes) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution 
+{
+    boolean dfs(List<List<Integer>> adj,int[] vis,int[] pathVis,int[] check,int node)
+    {
+        vis[node]=1;
+        pathVis[node]=1;
+        check[node]=0;
+        for(int it:adj.get(node))
+        {
+            if(vis[it]==0)
+            {
+                if(dfs(adj,vis,pathVis,check,it)) return true;
+            }
+            else if(pathVis[it]==1) return true;
+        }
+        check[node]=1;
+        pathVis[node]=0;
+        return false;
+    }
+
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) 
+    {
+        List<Integer> safe=new ArrayList<>();
+        int[] vis=new int[V];
+        int[] pathVis=new int[V];
+        int[] check=new int[V];
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==0)
+            {
+                dfs(adj,vis,pathVis,check,i);
+            }
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1) safe.add(i);
+        }
+        return safe;
+    }
+}
